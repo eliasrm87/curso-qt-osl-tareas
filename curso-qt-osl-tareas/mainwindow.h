57de03bbdac25fe0bc5c28b2c3ca4c6c74847c7c
Andres <el.lobo.4ever@gmail.com>
2014-03-07 14:33:59 +0000

    Final

--- curso-qt-osl-tareas/mainwindow.h
+++ curso-qt-osl-tareas/mainwindow.h
@@ -18,13 +18,23 @@ public:
     ~MainWindow();
     void loadCategorias();
     void loadTareas();
+    void loadEtiquetas();
+
+    // Tareas
+    void setFilaTareas(int fila);
+    int getFilaTareas();
+    void setColTareas(int col);
+    int getColTareas();
 
 private slots:
 
     void onAddTarea();
+    void onTareaTxtChanged(); // Si cambias el texto de la descripción de una tarea, se actualiza en la base de datos
 
     void onAddCateg(); // Añade una fila para rellenar una nueva categoria
     void onSaveCateg(int fila, int col); // Guarda en la base de datos los cambios realizados en las categorias
+
+    void onAddEtiq();
 
     void onTareasCellChanged(int row, int column);
     void onLoadTareas();
@@ -40,6 +50,9 @@ private:
     bool addingTarea_;
     bool addingCategoria_;
     bool addingEtiqueta_;
+
+    // Filas y columnas (globales)
+    int filaTareas_, colTareas_;
 };
 
 #endif // MAINWINDOW_H
