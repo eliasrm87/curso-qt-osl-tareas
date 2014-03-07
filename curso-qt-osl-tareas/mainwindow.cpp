8fea975d880244a503e347be9c07a6d08d7b0b80
Aaron Socas Gaspar <alu0100207385@ull.edu.es>
2014-03-07 12:38:40 +0000

    barra menu modificada1

--- curso-qt-osl-tareas/mainwindow.cpp
+++ curso-qt-osl-tareas/mainwindow.cpp
@@ -6,6 +6,48 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    //Setup menu
+    //Establecemos el tamaño inicial de la ventana
+    this->setMaximumSize(maximumWidth(),maximumHeight());
+
+    //Establecemos el título de la ventana
+    this->setWindowTitle(tr("Gestor de tareas (SQLite)"));
+    //setCentralWidget(this);
+
+    //Mostramos la vista de la aplicacion inicial centrada respecto a la pantalla
+    set_central_win();
+
+    //Inicializamos la barra de menus
+    mainMenu_= new QMenuBar(this);
+    setMenuBar(mainMenu_);
+
+    //Pestaña Archivo
+    mnuArchivo_= new QMenu(tr("&Archivo"),this);
+    mainMenu_->addMenu(mnuArchivo_);
+
+    actArchivoGuardar_= new QAction(tr("&Guardar"),this);
+    actArchivoGuardar_->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_G));
+    mnuArchivo_->addAction(actArchivoGuardar_);
+    connect(actArchivoGuardar_, SIGNAL(triggered()),this,SLOT(Guardar()));
+    mnuArchivo_->addSeparator();
+    actArchivoCerrar_ = new QAction(QIcon(":/iconos/Button-Close-icon.png"),tr("&Cerrar"),this);
+    actArchivoCerrar_->setShortcut(QKeySequence(Qt::CTRL + Qt::Key_C));
+    mnuArchivo_->addAction(actArchivoCerrar_);
+    connect(actArchivoCerrar_, SIGNAL(triggered()),this,SLOT(Cerrar()));
+
+/*    //Pestaña Ayuda
+    mnuAyuda_= new QMenu(tr("A&yuda"),this);
+    mainMenu_->addMenu(mnuAyuda_);
+
+    mnuAyudaDoc_ = new QMenu(tr("Documentacion"),this);
+    mnuAyuda_->addMenu(mnuAyudaDoc_);
+    Documentacion();
+    mnuAyuda_->addSeparator();
+
+    actAyudaAcercaDe_ = new QAction(QIcon(""),tr("AcercaDe"),this);
+    mnuAyuda_->addAction(actAyudaAcercaDe_);
+    connect(actAyudaAcercaDe_, SIGNAL(triggered()),this,SLOT(AcercaDe()));
+*/
 
     //Setup database
     ConecToDb(db_, "tareas");
@@ -63,8 +105,92 @@ MainWindow::MainWindow(QWidget *parent) :
 
 MainWindow::~MainWindow()
 {
+    mainMenu_->deleteLater();
+    mnuArchivo_->deleteLater();
+    actArchivoGuardar_->deleteLater();
+    actArchivoCerrar_->deleteLater();
+    mnuAyudaDoc_->deleteLater();
+    actAyudaAcercaDe_->deleteLater();
     delete ui;
 }
+
+
+void MainWindow::set_central_win()
+{
+    QDesktopWidget *desktop = QApplication::desktop();
+
+    int screenWidth, width;
+    int screenHeight, height;
+    int x, y;
+    QSize windowSize;
+
+    screenWidth = desktop->width();
+    screenHeight = desktop->height();
+
+    windowSize = size();
+    width = windowSize.width();
+    height = windowSize.height();
+
+    x = (screenWidth - width) / 2;
+    y = (screenHeight - height) / 2;
+    y -= 25;//barra_menus + barra_aplicacion (a OJO!)
+    move ( x, y );
+
+    setFixedSize(windowSize.width(), windowSize.height());
+}
+
+void MainWindow::Cargar()
+{
+
+}
+
+void MainWindow::Guardar()
+{
+
+}
+
+void MainWindow::Cerrar()
+{
+    QMessageBox msg(this);
+    msg.setInformativeText("¿Esta seguro de salir?");
+    msg.addButton("Si",QMessageBox::YesRole);
+    msg.addButton("No",QMessageBox::NoRole);
+
+    QMessageBox msg2(this);
+    msg2.setInformativeText("¿Desea guardar el trabajo actual?");
+    msg2.addButton("Si",QMessageBox::YesRole);
+    msg2.addButton("No",QMessageBox::NoRole);
+    if (!msg.exec())
+    {
+        if (!msg2.exec())
+            Guardar();
+        close();
+    }
+}
+
+void MainWindow::Documentacion()
+{
+    QAction* actAyudaDoc1;
+    actAyudaDoc1= new QAction(tr("Qt Documentacion"),this);
+    mnuAyudaDoc_->addAction(actAyudaDoc1);
+    connect(actAyudaDoc1, SIGNAL(triggered()),this,SLOT(abrirURL()));
+}
+
+void MainWindow::abrirURL(){
+    QDesktopServices::openUrl(QUrl("http://qt-project.org/doc/", QUrl::TolerantMode));
+}
+
+
+void MainWindow::AcercaDe()
+{
+    QMessageBox msg(this);
+    msg.setWindowTitle(tr("Acerca de"));
+    msg.setText("<p>Gestor de tareas (en Qt!)</p><p>v 1.0</p>");
+    msg.addButton("OK",QMessageBox::AcceptRole);
+    if (msg.exec())
+        close();
+}
+
 
 void MainWindow::onAddTarea()
 {
--- curso-qt-osl-tareas/mainwindow.h
+++ curso-qt-osl-tareas/mainwindow.h
@@ -5,6 +5,17 @@
 #include <QDebug>
 #include "dbconnection.h"
 
+#include <QSqlDatabase>
+
+#include <QMenu>
+#include <QMenuBar>
+#include <QAction>
+#include <QRect>
+#include <QDesktopWidget>
+#include <QApplication>
+#include <QDesktopServices>
+#include <QUrl>
+
 namespace Ui {
 class MainWindow;
 }
@@ -18,11 +29,28 @@ public:
     ~MainWindow();
 
 private slots:
+    void set_central_win();
+    void Cargar();
+    void Guardar();
+    void Cerrar();
+    void Documentacion();
+    void abrirURL();
+    void AcercaDe();
+
     void onAddTarea();
     void onTareasCellChanged(int row, int column);
     void onLoadTareas();
 
 private:
+    QMenuBar* mainMenu_;
+    QMenu* mnuArchivo_;
+    QMenu* mnuAyuda_;
+    QMenu* mnuAyudaDoc_;
+    QAction* actArchivoGuardar_;
+    QAction* actArchivoCerrar_;
+    QAction* actAyudaDocumentacion_;
+    QAction* actAyudaAcercaDe_;
+
     Ui::MainWindow *ui;
     QSqlDatabase db_;
     bool addingTarea_;
